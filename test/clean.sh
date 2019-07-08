60647bc45a51fbc5ed447c8a73d65bdc2cc3fa75
Tizen Ye <yzs981130@126.com>
2019-07-08 14:06:14 +0800

    add pcl test

---
+++ test/clean.sh
@@ -0,0 +1,3 @@
+kubectl delete -f nvidia.yaml
+kubectl delete -f nvidia2.yaml
+kubectl delete -f nvidia3.yaml
---
+++ test/start.sh
@@ -0,0 +1,3 @@
+kubectl create -f nvidia.yaml
+kubectl create -f nvidia2.yaml
+kubectl create -f nvidia3.yaml
